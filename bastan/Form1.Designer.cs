8a01a087b26e05b18d2d42d1ce9e33dce60c4509
resobyte <iresatakcan.bm@gmail.com>
2016-12-18 00:46:00 +0300

    Entropisiz Proje yüklendi.

---
+++ bastan/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace bastan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txb_Dosyayolu = new System.Windows.Forms.TextBox();
+            this.Btn_DosyaSec = new System.Windows.Forms.Button();
+            this.Btn_Veriduzelt = new System.Windows.Forms.Button();
+            this.Btn_satirsutunsayisi = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Txb_satirsayisi = new System.Windows.Forms.TextBox();
+            this.Txb_sutunsayisi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.857143F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(56, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dosya Seçin";
+            // 
+            // Txb_Dosyayolu
+            // 
+            this.Txb_Dosyayolu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.857143F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txb_Dosyayolu.Location = new System.Drawing.Point(59, 67);
+            this.Txb_Dosyayolu.Margin = new System.Windows.Forms.Padding(2);
+            this.Txb_Dosyayolu.Multiline = true;
+            this.Txb_Dosyayolu.Name = "Txb_Dosyayolu";
+            this.Txb_Dosyayolu.Size = new System.Drawing.Size(216, 43);
+            this.Txb_Dosyayolu.TabIndex = 4;
+            // 
+            // Btn_DosyaSec
+            // 
+            this.Btn_DosyaSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Btn_DosyaSec.Location = new System.Drawing.Point(59, 142);
+            this.Btn_DosyaSec.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_DosyaSec.Name = "Btn_DosyaSec";
+            this.Btn_DosyaSec.Size = new System.Drawing.Size(86, 28);
+            this.Btn_DosyaSec.TabIndex = 5;
+            this.Btn_DosyaSec.Text = "Dosya Seç";
+            this.Btn_DosyaSec.UseVisualStyleBackColor = true;
+            this.Btn_DosyaSec.Click += new System.EventHandler(this.Btn_DosyaSec_Click);
+            // 
+            // Btn_Veriduzelt
+            // 
+            this.Btn_Veriduzelt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Btn_Veriduzelt.Location = new System.Drawing.Point(59, 206);
+            this.Btn_Veriduzelt.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_Veriduzelt.Name = "Btn_Veriduzelt";
+            this.Btn_Veriduzelt.Size = new System.Drawing.Size(106, 35);
+            this.Btn_Veriduzelt.TabIndex = 6;
+            this.Btn_Veriduzelt.Text = "Verileri Düzelt";
+            this.Btn_Veriduzelt.UseVisualStyleBackColor = true;
+            this.Btn_Veriduzelt.Click += new System.EventHandler(this.Btn_Veriduzelt_Click);
+            // 
+            // Btn_satirsutunsayisi
+            // 
+            this.Btn_satirsutunsayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Btn_satirsutunsayisi.Location = new System.Drawing.Point(59, 269);
+            this.Btn_satirsutunsayisi.Margin = new System.Windows.Forms.Padding(2);
+            this.Btn_satirsutunsayisi.Name = "Btn_satirsutunsayisi";
+            this.Btn_satirsutunsayisi.Size = new System.Drawing.Size(104, 50);
+            this.Btn_satirsutunsayisi.TabIndex = 7;
+            this.Btn_satirsutunsayisi.Text = "Satır-Sutun Sayılarını Bul";
+            this.Btn_satirsutunsayisi.UseVisualStyleBackColor = true;
+            this.Btn_satirsutunsayisi.Click += new System.EventHandler(this.Btn_satirsutunsayisi_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(221, 269);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Satır Sayısı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(221, 304);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Sütün Sayısı";
+            // 
+            // Txb_satirsayisi
+            // 
+            this.Txb_satirsayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txb_satirsayisi.Location = new System.Drawing.Point(321, 269);
+            this.Txb_satirsayisi.Margin = new System.Windows.Forms.Padding(2);
+            this.Txb_satirsayisi.Name = "Txb_satirsayisi";
+            this.Txb_satirsayisi.Size = new System.Drawing.Size(56, 21);
+            this.Txb_satirsayisi.TabIndex = 10;
+            // 
+            // Txb_sutunsayisi
+            // 
+            this.Txb_sutunsayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txb_sutunsayisi.Location = new System.Drawing.Point(321, 304);
+            this.Txb_sutunsayisi.Margin = new System.Windows.Forms.Padding(2);
+            this.Txb_sutunsayisi.Name = "Txb_sutunsayisi";
+            this.Txb_sutunsayisi.Size = new System.Drawing.Size(56, 21);
+            this.Txb_sutunsayisi.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 396);
+            this.Controls.Add(this.Txb_sutunsayisi);
+            this.Controls.Add(this.Txb_satirsayisi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Btn_satirsutunsayisi);
+            this.Controls.Add(this.Btn_Veriduzelt);
+            this.Controls.Add(this.Btn_DosyaSec);
+            this.Controls.Add(this.Txb_Dosyayolu);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txb_Dosyayolu;
+        private System.Windows.Forms.Button Btn_DosyaSec;
+        private System.Windows.Forms.Button Btn_Veriduzelt;
+        private System.Windows.Forms.Button Btn_satirsutunsayisi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Txb_satirsayisi;
+        private System.Windows.Forms.TextBox Txb_sutunsayisi;
+    }
+}
+
---
+++ bastan/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace bastan
+{
+    public class karsilastir 
+    {
+        public string kelime;
+        public int sayi;
+    }
+    public class sayi_tut
+    {
+        public int sayi;
+    }
+
+    public partial class Form1 : Form
+    {
+        List<karsilastir> liste = new List<karsilastir>();
+        List<sayi_tut> txt_sayilar = new List<sayi_tut>();
+       
+        int say=1;
+        public Form1()
+        {
+            InitializeComponent();
+           //int say = 1;
+        }
+        /*Dosya seçimi için filtreleme*/
+        private void Btn_DosyaSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog()
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop), // dosya seçici pencere masaüstünden başlıyor
+                Filter = "Excel Dosyası | *.xlsx |Excel Dosyası |*.xls |Csv Dosyası| *.csv",
+                FilterIndex = 3
+            };
+
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                string DosyaYolu = openfile.FileName;
+                string DosyaAdi = openfile.SafeFileName;
+
+                Txb_Dosyayolu.Text = DosyaYolu; // Ana sayfadaki txt_dosyayolu metin kutusuna dosya yolunu yaz
+            }
+        }
+
+        /* değerin String mi sayısal değer olduğunu bulma*/
+        public static bool IsNumeric(string text)
+        {
+            double test;
+            return double.TryParse(text, out test);
+        }
+
+        /*Seçilen Dosyayı Okuma*/
+       
+        private String[,] oku()
+        {
+            var dosyaOkuyucu = new StreamReader(File.OpenRead(Txb_Dosyayolu.Text));
+                   
+            var satirSayisi = SatirSayisiBul();
+            var sutunSayisi = SutunSayisiBul();
+            double[,] sondizi = new double[satirSayisi - 1, sutunSayisi - 1];
+
+            string[,] veriler = new string[(satirSayisi - 1), (sutunSayisi - 1)];
+
+            // Dosyanın sadece ilk satırının okunup, özellik adlarının ataması (dosyanın ilk satırını atla)
+            var ilksatir = dosyaOkuyucu.ReadLine();
+            var ozellikler = ilksatir.Split(',');
+            /* NA lar değişecek ve dosyadaki sayı değerleri ile aynı olmayacak*/
+            for (int i = 0; i < (satirSayisi - 1); i++) // satırları oku
+            {
+                var satir = dosyaOkuyucu.ReadLine(); // StreamReader nesnesi
+                for (int j = 1; j < (sutunSayisi ); j++) // sütunları oku
+                {
+                    var degerler = satir.Split(','); // satırdaki verileri virgüle göre ayır
+                  
+                    /*iç for*/
+                    sayi_tut tut = new sayi_tut();
+                    if(IsNumeric(degerler[j]))
+                    {
+                        tut.sayi = Convert.ToInt32(degerler[j]);
+                        txt_sayilar.Add(tut);
+                    }
+                    veriler[i, j-1] = degerler[j];
+                    
+                }
+                /*dış for*/
+            }
+            return veriler;
+            /*fonk bitis*/
+
+        }
+      /*Stringleri int çevirme*/
+
+        private string[,] veri_duzelt(String[,] veriler)
+        {
+            int sayi_ata = 1;
+            var satirSayisi = SatirSayisiBul();
+            var sutunSayisi = SutunSayisiBul();
+            for(int i=0;i<(satirSayisi-1);i++) //satır
+            {
+                for(int j=0;j<(sutunSayisi-1);j++)
+                {
+                    karsilastir ata = new karsilastir();
+                    int kontrol = 0;
+                    if (IsNumeric(veriler[i,j]) == false && veriler[i,j] != "NA") //değer sayısal değilse
+                    {
+                        if (say != 1)
+                        {
+                            foreach (var dolas in liste)
+                            {
+                                if (String.Equals(dolas.kelime, veriler[i,j]) == true) /*okunan string ile class taki değerler karşılaşrıma eşit ise sayı otomatik olarak atanıyor*/
+                                {
+                                    sayi_ata = dolas.sayi;
+                                    kontrol++;
+                                    break;
+                                }
+
+                            }
+                            if (kontrol == 0)/*classtaki stringler ile eşleşen yoksa yeni sayı atanıyor.*/
+                            {
+                                ata.kelime = veriler[i,j];
+                            
+                            etiket:
+                                foreach (var sayi_kontrol in txt_sayilar) /*dosyadaki int değerler ile çakışma olmaması için yeni class ile kontrol ediliyor.*/
+                                {
+                                    if (sayi_kontrol.sayi == say)
+                                    {
+                                        say++;
+                                        goto etiket;
+                                    }
+                                }
+                                ata.sayi = say;
+                                sayi_ata = say;
+                                liste.Add(ata);
+                                say++;
+                            }
+                        }
+                     
+                          else
+                        {
+                        
+                            ata.kelime = veriler[i,j];
+                            etiket:
+                            foreach (var sayi_kontrol in txt_sayilar)/*dosyadaki int değerler ile çakışma olmaması için yeni class ile kontrol ediliyor.*/
+                            {
+                                if (sayi_kontrol.sayi == say)
+                                { 
+                                    say++;
+                                    goto etiket;
+                                }
+                            }
+                            ata.sayi = say;
+                            sayi_ata = say;
+                            liste.Add(ata);
+                            say++;
+                        }
+                        veriler[i, j] = sayi_ata.ToString();
+                    }
+                    else
+                    {
+                        veriler[i, j] = veriler[i,j];      // ayırılan değerleri matrise al
+                    }
+
+                    
+
+                }
+            }
+
+            for (int i = 0; i < (satirSayisi - 1);i++ )
+            {
+                for(int j=0;j<(sutunSayisi-2);j++)
+                {
+                    if(veriler[i,j]=="NA")//NA ları bulup ortalaması alınması için NA_ortalama fonk. yollandı.
+                    {
+                        veriler[i, j] = NA_ortalama(veriler, j).ToString();
+                    }
+                }
+            }
+
+                return veriler;
+        }
+        
+        
+        /*NA ortalama */
+        private int NA_ortalama(string[,] dizi, int sutun)
+        {
+            int ortalama = 0;
+            int toplam = 0;
+            int adet = 0;
+            int satirSayisi = SatirSayisiBul();
+
+
+            for (int i = 0; i < satirSayisi - 1; i++)
+            {
+                for (int j = sutun; j < sutun + 1; j++)
+                {
+                    if (dizi[i, j] != "NA")
+                    {
+                        toplam += Convert.ToInt32(dizi[i, j]);
+                        adet++;
+                    }
+                }
+
+            }
+            ortalama = toplam / adet;
+
+            return ortalama;
+        }
+        
+        /*Txt yazma*/
+        private void yaz(String[,] veriler)
+        {
+            string dosya_yolu = @"D:\metinbelgesi.txt";
+
+            FileStream fs = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Write);
+
+            StreamWriter sw = new StreamWriter(fs);
+            int satirSayisi = SatirSayisiBul();
+            int sutunSayisi = SutunSayisiBul();
+
+            for (int i = 0; i < satirSayisi - 1; i++)
+            {
+
+                for (int j = 0; j < sutunSayisi - 1; j++)
+                {
+                    sw.Write(veriler[i, j] + ",");
+
+                }
+                sw.Write("\n");
+            }
+
+            sw.Flush();
+
+            sw.Close();
+            fs.Close();
+        }
+
+        #region Satır ve Sütun Sayılarını Bulan Fonksiyonların Tanımlamaları
+        public int SutunSayisiBul()
+        {
+            var lines = File.ReadAllLines(Txb_Dosyayolu.Text);
+            var sutunsayisi = lines[0].Split(',').Length;
+
+            return sutunsayisi;
+        }
+        public int SatirSayisiBul()
+        {
+            var lines = File.ReadAllLines(Txb_Dosyayolu.Text);
+            var satirsayisi = lines.Count();
+
+            return satirsayisi;
+        }
+        #endregion
+
+        private void Btn_satirsutunsayisi_Click(object sender, EventArgs e)
+        {
+            Txb_satirsayisi.Text = SatirSayisiBul().ToString();
+            Txb_sutunsayisi.Text = SutunSayisiBul().ToString(); 
+        }
+
+        private void Btn_Veriduzelt_Click(object sender, EventArgs e)
+        {
+            /*
+              var iki = oku();
+              yaz(veri_duzelt(iki));
+              */
+            var iki = veri_duzelt(oku());
+           
+        }
+
+        /*id3 tree algoritması*/
+
+    
+    }
+}
